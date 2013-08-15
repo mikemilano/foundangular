e77b57948a2a1ecabd986c1849a7fa875520a65f
Mike Milano <coder1@gmail.com>
2013-08-14 21:36:52 -0700

    Update for dropdown.

--- foundangular.js
+++ foundangular.js
@@ -1,9 +1,11 @@
 'use strict'
 
-angular.module('foundangular', []).
-directive('alert', function(){
+angular.module('foundangular', [])
+
+// Alert component
+.directive('zfAlert', function(){
   return {
-    restrict: "EA",
+    restrict: "E",
     replace: true,
     transclude: true,
     scope: {
@@ -11,7 +13,6 @@ directive('alert', function(){
       noClose: "@"
     },
     controller: function($scope) {
-      console.log(angular.isDefined($scope.noClose));
       $scope.remove = function() {
         $scope.element.remove();
       }
@@ -23,4 +24,77 @@ directive('alert', function(){
       scope.hideClose = "noClose" in attrs;
     }
   }
-});
+})
+
+.directive('alert', function(){
+  return {
+    restrict: "A",
+    replace: true,
+    transclude: true,
+    scope: {
+      closeText: "@",
+      noClose: "@"
+    },
+    controller: function($scope) {
+      $scope.remove = function() {
+        $scope.element.remove();
+      }
+    },
+    template: '<div class="alert-box"><span ng-transclude></span><a ng-hide="hideClose" class="close" ng-click="remove()">{{ closeText || "&times;" }}</a></div>',
+    link: function(scope, element, attrs) {
+      element.addClass(attrs.class);
+      scope.element = element;
+      scope.hideClose = "noClose" in attrs;
+    }
+  }
+})
+
+
+// Dropdown component
+.factory('DropdownData', [function(){
+  return {elements: {}}
+}])
+
+.directive('dropdown', function(){
+  return {
+    restrict: "A",
+    scope: {
+      dropdown: '@'
+    },
+
+    controller: ['$scope', 'DropdownData', function($scope, DropdownData) {
+      DropdownData[$scope.dropdown] = {
+        activeClass: 'open',
+        is_hover: false
+      }
+      $scope.data = DropdownData[$scope.dropdown];
+      $scope.open = function (dropdown, target) {
+        // Add code to display dropdown
+      }
+      $scope.close = function (dropdown) {
+        // Add code to hide dropdown
+      }
+    }],
+
+    link: function(scope, element) {
+      scope.data.triggerElement = element;
+      element.bind('click', function(e) {
+        scope.open(scope.data.contentElement, scope.data.triggerElement);
+      });
+    }
+  }
+})
+
+.directive('dropdownContent', function(){
+  return {
+    scope: {
+      id: '@'
+    },
+    controller: ['$scope', 'DropdownData', function($scope, DropdownData){
+      $scope.data = DropdownData[$scope.id];
+    }],
+    link: function(scope, element) {
+      scope.data.contentElement = element;
+    }
+  }
+})
